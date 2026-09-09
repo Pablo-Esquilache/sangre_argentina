fc4ab18ac318f3f715774d1d32026abc2972f6a5
Pablo <pabloesquilache@gmail.com>
2026-09-08 23:01:50 -0300

    Fix: Escapar comillas en JSX para solucionar error de compilación

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className={styles.info}>
           <h3>Sangre Argentina</h3>
           <p className={styles.quote}>
-            "Lo que pinta este pincel ni el tiempo lo ha de borrar, ninguno se ha de animar a corregirme la plana, no pinta quien tiene gana sino quien sabe pintar".
+            &ldquo;Lo que pinta este pincel ni el tiempo lo ha de borrar, ninguno se ha de animar a corregirme la plana, no pinta quien tiene gana sino quien sabe pintar&rdquo;.
           </p>
         </div>
         
