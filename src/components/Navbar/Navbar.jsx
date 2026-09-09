7a01f9a1a5056ddc25beacc21977c259248478e8
Pablo <pabloesquilache@gmail.com>
2026-09-09 00:44:25 -0300

    Convertir fondo negro a blanco en el nuevo logo y actualizar referencias a logo.png

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
         
         <div className={styles.leftSection}>
           <Link href="/#inicio" className={styles.logoLink} onClick={closeMenu}>
-            <Image src="/logo.jpg" alt="Logo Sangre Argentina" width={55} height={55} className={styles.logoImg} />
+            <Image src="/logo.png" alt="Logo Sangre Argentina" width={55} height={55} className={styles.logoImg} />
           </Link>
           <div className={styles.search}>
             <input type="text" placeholder="Buscar entrevistas..." />
